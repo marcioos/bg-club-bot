4a0c1d80901444dc6d42d4890302adcf9322b148
Marcio de Oliveira da Silva <marcio.oliveira@klarna.com>
2017-05-22 16:44:44 +0200

    Add you-tube module (WIP)

---
+++ app/you-tube.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+
+const REQUEST_TIMEOUT_MS = 10000
+
+const axiosInstance = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  timeout: REQUEST_TIMEOUT_MS,
+})
+
+module.exports = {
+  getRulesVideoUrl: function (gameName) {
+    const queryTerm = encodeURI(`${gameName} rules`)
+
+    return axiosInstance.get(`/search?q=${queryTerm}&part=snippet&maxResults=1`)
+      .then((response) => response)
+      .catch((error) => console.log(error))
+  }
+}
