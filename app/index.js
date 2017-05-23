0d81a891b17e0018b31b5bd2f5e402fca98d3533
Marcio de Oliveira da Silva <marcio.oliveira@klarna.com>
2017-05-23 23:08:25 +0200

    Use port provided by heroku

--- app/index.js
+++ app/index.js
@@ -18,4 +18,6 @@ app.post('/propose/:bggId', async (req, res) => {
   }
 })
 
-app.listen(3000, () => console.log('bg-club-bot started on port 3000'))
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log('bg-club-bot started on port ' + PORT))
