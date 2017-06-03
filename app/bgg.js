1706b7fea5318a79f8566f3ea7d9881d0fc6c94e
Marcio de Oliveira da Silva <marcio.oliveira@klarna.com>
2017-06-03 17:19:12 +0900

    Make bgg function more xmlapi2 proof

--- app/bgg.js
+++ app/bgg.js
@@ -1,20 +1,42 @@
 const bggClient = require('bgg-axios')
 
+function getGameName (game) {
+  if (Array.isArray(game.name)) {
+    return game.name
+      .filter((name) => name.type === 'primary')
+      .map((name) => name.value)[0]
+  }
+  return game.name.value
+}
+
+function getNumPlayers (game) {
+  const minPlayers = game.minplayers.value
+  const maxPlayers = game.maxplayers.value
+  if (minPlayers === '0' && maxPlayers === '0') {
+    return null
+  }
+  return minPlayers !== maxPlayers ? `${minPlayers}-${maxPlayers}` : maxPlayers
+}
+
+function getPlayTime (game) {
+  const playingTime = game.playingtime.value
+  const minPlayTime = game.minplaytime.value
+  const maxPlayTime = game.maxplaytime.value
+  if (minPlayTime === '0' && maxPlayTime === '0' && playingTime === '0') {
+    return null
+  }
+  return (minPlayTime !== maxPlayTime ? `${minPlayTime}-${maxPlayTime}` : playingTime) + ' min'
+}
+
 module.exports = {
   fetchBggProposalData: (bggId) => {
     return bggClient.apiRequest('thing', { id: bggId })
       .then((bggResponse) => {
         const game = bggResponse.items.item
-        const minPlayers = game.minplayers.value
-        const maxPlayers = game.maxplayers.value
-        const playingTime = game.playingtime.value
-        const minPlayTime = game.minplaytime.value
-        const maxPlayTime = game.maxplaytime.value
-
         return {
-          gameName: game.name.value,
-          numPlayers: minPlayers !== maxPlayers ? `${minPlayers}-${maxPlayers}` : maxPlayers,
-          playTime: (minPlayTime !== maxPlayTime ? `${minPlayTime}-${maxPlayTime}` : playingTime) + ' min',
+          gameName: getGameName(game),
+          numPlayers: getNumPlayers(game),
+          playTime: getPlayTime(game),
           bggUrl: `https://boardgamegeek.com/boardgame/${bggId}`
         }
       })
