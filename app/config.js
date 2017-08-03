dab1da4b2991ae02f127ac4d8bb14aad1ec843c1
Marcio de Oliveira da Silva <marcio.oliveira@klarna.com>
2017-08-03 10:01:25 +0200

    Add config file

--- app/config.js
+++ app/config.js
@@ -1,5 +1,9 @@
+const PORT = process.env.PORT || 3000
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || 'telegram-test-token'
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || 'google-test-token'
+
 module.exports = {
-  PORT: process.env.PORT || 3000
-  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || 'telegram-test-token'
-  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || 'google-test-token'
+  PORT,
+  TELEGRAM_BOT_TOKEN,
+  GOOGLE_API_KEY
 }
